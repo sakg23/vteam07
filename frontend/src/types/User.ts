9b0bbf5b25b1d862e2595579ee2fc6f3d8d1b62b
sakg23 <sakg23@student.bth.se>
2025-05-28 22:17:00 +0200

    Remove frontend folder

--- frontend/src/types/User.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  balance: number;
-  role: 'customer' | 'admin';
-  createdAt: string;
-}
--- frontend/src/types/zone.ts
+++
@@ -1,7 +0,0 @@
-export interface Zone {
-  id: number;
-  name: string;
-  city: string;
-  type: 'allowed' | 'restricted';
-  polygon: [number, number][];
-}
